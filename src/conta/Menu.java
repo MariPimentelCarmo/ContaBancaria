11c53ac8e7f81a9e6671e58ab1a7aa599049a3de
MariPimentelCarmo <mariana.carmo@sptech.school>
2025-02-18 21:11:19 -0300

    TASK-03 – Construção das Classes Model ContaCorrente Conta Poupanca como Herança da Classe Conta

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -6,7 +6,7 @@ import conta.model.ContaPoupanca;
 
 import java.util.Scanner;
 import conta.util.Cores;
-import conta.model.Conta;
+
 
 public class Menu {
 	public static void main(String[] args) {
@@ -15,13 +15,6 @@ public class Menu {
 
 		int opcao;
 
-		// Teste da Classe Conta
-		Conta c1 = new Conta(3, 123, 1, "Mariana", 500000.0f);
-		c1.visualizar();
-		c1.sacar(12000.0f);
-		c1.visualizar();
-		c1.depositar(5000.0f);
-		c1.visualizar();
 
 		// Teste da Classe Conta Corrente
 		ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "José da Silva", 0.0f, 1000.0f);
